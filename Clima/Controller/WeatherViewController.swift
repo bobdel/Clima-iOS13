a3fdfae458b92b1aa18402ecbe42f1ea7e7a0703
Robert DeLaurentis <lists@bobdel.com>
2019-10-31 16:59:44 -0700

    Completed through Use URLSessionForNetworking chapter. The URLSession gets data, but note how the code is written in WeatherManager without a closure.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    let weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        searchTextField.delegate = self // set this ViewController as the delegate for the search box
     }
 
-
+    /// Handle Search button
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    // MARK: TextField Delegate Methods
+    
+    /// Handle Go button on keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    /// Do a little validation first
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            searchTextField.placeholder = "Type Something"
+            return false
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        weatherManager.fetchWeather(cityName: textField.text!)
+        searchTextField.text = ""
+    }
+    
 }
 
---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,46 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Robert DeLaurentis on 10/31/19.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather/appid=5f445712a430c6ec4dd43d6fd7fbc419&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!)
+        }
+    }
+}
